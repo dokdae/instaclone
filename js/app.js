a60339bd72eba0ede40de56fc06f3e03007e9f9f
dokdae <kimdaekeon@gmail.com>
2017-07-23 15:58:31 +0900

    Fuck body!

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 var drinks = ["soju", "beer", "whisky"];
+
+var dork = {
+  name: "Chester",
+  age: 41,
+  suicide: true,
+  drink: drinks[2]
+};
+
+function multiply(one, two) {
+  return one * two;
+}
+
+function divide(one, two) {
+  return one / two;
+}
+
+function sum(one, two) {
+  return one + two;
+}
+
+function substract(one, two) {
+  return one - two;
+}
