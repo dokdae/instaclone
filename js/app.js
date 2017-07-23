b520c7f118df51513e6bda2561475e8ea732b79b
dokdae <kimdaekeon@gmail.com>
2017-07-23 16:10:11 +0900

    JS!

--- js/app.js
+++ js/app.js
@@ -1,24 +1,36 @@
 var drinks = ["soju", "beer", "whisky"];
 
-var dork = {
-  name: "Chester",
-  age: 41,
-  suicide: true,
-  drink: drinks[2]
+var soldier = {
+  name: "Nicolas",
+  age: 57,
+  korean: false,
+  drink: drinks[2],
+  sayHi: function(name) {
+    console.log("Hello" + name);
+  }
 };
 
-function multiply(one, two) {
-  return one * two;
+var age = prompt("How old are you?");
+
+if (age > 18) {
+  console.log("drink!");
+} else {
+  console.log("GTFO!!!");
 }
 
-function divide(one, two) {
-  return one / two;
-}
-
-function sum(one, two) {
-  return one + two;
-}
-
-function substract(one, two) {
-  return one - two;
-}
+//
+// function multiply(one, two) {
+//   return one * two;
+// }
+//
+// function divide(one, two) {
+//   return one / two;
+// }
+//
+// function sum(one, two) {
+//   return one + two;
+// }
+//
+// function substract(one, two) {
+//   return one - two;
+// }
